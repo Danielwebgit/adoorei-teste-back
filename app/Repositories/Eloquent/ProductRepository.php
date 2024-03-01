d6c3dac68187ac14074660d26c1b9f697d05dc42
Danielwebgit <nielferas@gmail.com>
2024-03-01 14:47:50 -0300

    Estrutura Service e Repository para produtos

---
+++ app/Repositories/Eloquent/ProductRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Repositories\Contracts\ProductRepositoryInterface;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    public function fetchProducts()
+    {
+
+    }
+}
---
+++ app/Services/ProductServices.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class ProductServices
+{
+    public function __construct()
+    {
+
+    }
+}
